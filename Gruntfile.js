08ab0345c499a76b72c8c051a6b3fdb1376fb560
Mikhail <tradecontent@gmail.com>
2019-01-17 00:01:14 +0800

    Build tools, readme

---
+++ Gruntfile.js
@@ -0,0 +1,96 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    cssmin: {
+      target: {
+        src : "source/<%= pkg.name %>.css",
+        dest : "s.css"
+      }
+    },
+    jshint: {
+      files: ['source/<%= pkg.name %>.js'],
+      options: {
+          esnext: true
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %>\n<%= pkg.homepage %> <%= grunt.template.today("yyyy-mm-dd") %> */'
+      },
+      build: {
+        src: 'source/<%= pkg.name %>.js',
+        dest: 'source/build/j.min.js'
+      }
+    },
+    'string-replace': {
+      inline: {
+        files: {
+          'source/build/h.html': 'source/<%= pkg.name %>.html',
+        },
+        options: {
+          replacements: [
+            {
+              pattern: '<script src="./mkrl.js"></script>',
+              replacement: '<script><%= grunt.file.read("./source/build/j.min.js") %></script>'
+            },
+            {
+              pattern: '<link rel="stylesheet" href="./mkrl.css">',
+              replacement: '<link rel="stylesheet" href="./s.css">'
+            },
+            {
+              pattern: '<script src="../plainterm/build/plainterm.js.min.js"></script>',
+              replacement: '<script src="./plainterm/build/plainterm.js.min.js"></script>'
+            },
+            {
+              pattern: '<link rel="stylesheet" href="../plainterm/build/plainterm.js.min.css">',
+              replacement: '<link rel="stylesheet" href="./plainterm/build/plainterm.js.min.css">'
+            }
+          ]
+        }
+      },
+      size: {
+        files: {
+          'README.md': 'source/<%= pkg.name %>.md',
+        },
+        options: {
+          replacements: [
+            {
+              pattern: '<<size>>',
+              replacement: '<%= grunt.file.read("./source/build/size") %>'
+            }
+          ]
+        }
+      }
+    },
+    file_info: {
+      source_files: {
+        src: ['index.html', 's.css', 'plainterm/build/plainterm.js.min.css', 'plainterm/build/plainterm.js.min.js'],
+        options: {
+          inject: {
+            dest: './source/build/size',
+            text: '{{= sizeText(size(src[0])+size(src[1])+size(src[2])+size(src[3])) }} ({{= sizeText(gzipSize(src[0])+gzipSize(src[1])+gzipSize(src[2])+gzipSize(src[3])) }} gzipped)'
+          }
+        }
+      }
+    },
+    htmlmin: {                                    
+     dist: {                                      
+       options: {                                 
+         removeComments: true,
+         collapseWhitespace: true
+       },
+       files: {                                   
+         'index.html': 'source/build/h.html'
+       }
+     }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-string-replace');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-file-info');
+  grunt.registerTask('default', ['jshint','uglify', 'cssmin', 'string-replace:inline', 'htmlmin', 'file_info', 'string-replace:size']);
+};
---
+++ README.md
@@ -0,0 +1,25 @@
+## A homepage of mine
+
+Today is the era of previously unreachable download speeds, unlimited bandwidth and constantly increasing computing resources.
+With an incredibly large amount of third-party JavaScript libraries we sometimes tend to forget how to write pure, simple and lightweight code for simple tasks.
+
+This website is more of a personal reminder to myself: do not overcomplicate things.
+
+### Stuff
+
+[plainterm.js](https://github.com/mkrl/plainterm.js) - pure JS lightweight terminal "emulator"
+
+[node.js](https://nodejs.org/) running [grunt](https://gruntjs.com/) as build tools.
+
+Modify source @ `/source/mkrl.[file_extention]`
+
+Build with:
+
+```
+npm install
+grunt
+```
+
+Check out the gruntfile for build steps details.
+
+A total amount of traffic required to download and view this site is: 11.7 kB (5 kB gzipped), including all the code, design, logo and a JavaScript library.
