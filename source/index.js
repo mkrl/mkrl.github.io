29c9db240e679f38ad422c2a689d7da4a65a199b
Mikhail Korolev <self@mkrl.xyz>
2023-07-15 17:29:18 +0200

    Implement absolute `cd` and `ls`

--- source/index.js
+++ source/index.js
@@ -1,10 +1,10 @@
 import { initTerminal } from 'ttty'
-import { buildCommands } from './commands'
+import { cd, DEFAULT_PROMPT, ls } from './commands'
 
 const terminal = initTerminal({
     host: document.querySelector('#terminal'),
     welcomeMessage: `Hi. Make yourself at home. Don't hesitate to ask for help if you need it.`,
-    prompt: 'guest@mkrl.xyz:~$ ',
+    prompt: DEFAULT_PROMPT,
     commands: {
         about: {
             name: 'about',
@@ -13,43 +13,18 @@ const terminal = initTerminal({
                 type('Hi. I\'m Mikhail. I\'m a software engineer. This is my home page. By default you won\'t be greeted with a logotype, fancy design or a catchy title. If you need something, you can directly ask for the information you need, the good ol CLI way. This entire website is (probably) under 8 kb (under 4 gzipped). Whoa!',16)
             }
         },
-        norris: {
-            name: 'norris',
-            description: 'fetch a Chuck Norris joke',
-            func: ({ stop, start, print }) => {
-                start()
-                fetch('https://api.icndb.com/jokes/random?limitTo=[nerdy]')
-                    .then(data=> data.json())
-                    .then(res=> print(res.value.joke))
-                    .catch(error=> print(error))
-                    .finally(() => stop())
-            }
+        cd: {
+            name: 'cd',
+            description: 'navigate to a directory',
+            func: cd,
         },
-        contact: {
-            name: 'contact',
-            description: 'find ways to contact me',
-            func: ({ print }) => {
-                print(
-                    '<a href="mailto:self@mkrl.xyz">self@mkrl.xyz</a><br><a href="https://t.me/mkrlk" target="blank">Telegram</a><br><a href="https://github.com/mkrl/" target="blank">GitHub</a><br><a href="https://stackoverflow.com/users/7753036/mkrl" target="blank">Stack Overflow</a><br><a href="https://dev.to/mkrl" target="blank">dev.to</a>'
-                )}
-        },
-        keys: {
-            name: "keys",
-            description: "public ssh keys",
-            func: ({ print }) => {
-                print('<a href="/keys/work.pub" target="blank">work.pub</a><br><a href="/keys/server.pub" target="blank">server.pub</a>')
-            }
-        },
-        repo: {
-            name: 'repo',
-            description: 'source code',
-            func: ({ print }) => {
-                print('<a href="https://github.com/mkrl/mkrl.github.io" target="blank">Click</a>')
-            }
+        ls: {
+            name: 'ls',
+            description: 'list directory contents',
+            func: ls,
         },
     }
 })
 
-buildCommands()
 
 terminal.input.focus()
