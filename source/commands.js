86709eb720cefea125bad06f01b0b7262bfa2255
Mikhail Korolev <self@mkrl.xyz>
2023-07-15 17:29:18 +0200

    Implement absolute `cd` and `ls`

--- source/commands.js
+++ source/commands.js
@@ -1,8 +1,16 @@
 import fs from './fs.json'
-import { cond, startsWith, stubTrue } from 'lodash/fp'
+import { cond, startsWith, stubTrue, map } from 'lodash/fp'
 
 let currentPath = '/'
 
+const USERNAME = 'guest@mkrl.xyz'
+export const DEFAULT_PROMPT = `${USERNAME}:~$ `
+
+const formatAccessError = (command, path) =>
+  `${command}: no such file or directory: ${path}`
+
+const formatDirItem = (item) =>
+  `<span class="ls-item${item.type === 'dir' ? ' ls-dir' : ''}">${item.name}</span>`
 
 const resolveAbsolutePath = path => {
 
@@ -27,6 +35,23 @@ const navigate = (path) => {
 }
 
 
-export const buildCommands = () => {
-  console.log(fs.entries)
+export const cd = (terminal, ...args) => {
+  const newPath = args[0] ?? '/'
+  terminal.settings.prompt = `${USERNAME}:${newPath}$ `
+  currentPath = newPath
+}
+
+export const ls = ({ print }, ...args) => {
+  // TODO: handle -la shell-like flags
+  const path = args.length === 0 ? currentPath : args[0]
+
+
+  try {
+    print(
+      map(formatDirItem, fs[path].entries)
+        .join('')
+    )
+  } catch (e) {
+    print(formatAccessError('ls', path))
+  }
 }
