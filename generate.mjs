9676647bc28bff889a3c31f6fb780e0e3a2efea9
Mikhail Korolev <self@mkrl.xyz>
2023-07-20 23:46:07 +0200

    Added URL support to the filesystem + my relevant info

--- generate.mjs
+++ generate.mjs
@@ -3,6 +3,7 @@ import {
   lstat,
   readdir,
   writeFile,
+  readFile,
   access
 } from 'fs/promises'
 import path from 'path'
@@ -19,6 +20,18 @@ async function exists(file) {
   }
 }
 
+async function getUrlFile(file, extension) {
+  if (extension !== 'url') {
+    return {}
+  }
+  const contents = await readFile(file, 'utf-8')
+  const [description, url] = contents.split('\n')
+  return {
+    url,
+    description,
+  }
+}
+
 async function walkDirectory(dir) {
   const objMap = {}
   const traverse = async (entry) => {
@@ -30,13 +43,17 @@ async function walkDirectory(dir) {
     const absolutePath = path.basename(entry) === 'fs' ? '/' : `/${entry.slice(FS_DIR.length)}`
 
     if (!stats.isDirectory()) {
+      const extension = path.extname(entry).slice(1)
+      const urlData = await getUrlFile(entry, extension)
       objMap[absolutePath] = {
         name: path.basename(entry),
-        type: path.extname(entry).slice(1)
+        type: extension,
+        ...urlData,
       }
       return {
         name: path.basename(entry),
-        type: path.extname(entry).slice(1)
+        type: extension,
+        ...urlData,
       }
     }
 
@@ -44,15 +61,19 @@ async function walkDirectory(dir) {
     const childEntries = await Promise.all(
       files.map((child) => traverse(path.join(entry, child)))
     )
+    const hasReadme = childEntries.some((file) => file.name === 'README.txt')
+    const readmeFile = hasReadme && await readFile(path.join(entry, 'README.txt'), 'utf-8')
 
     objMap[absolutePath] = {
       name: path.basename(entry),
       type: 'dir',
+      description: readmeFile,
       entries: childEntries
     }
     return {
       name: path.basename(entry),
       type: 'dir',
+      description: readmeFile,
       path: absolutePath,
     }
   }
